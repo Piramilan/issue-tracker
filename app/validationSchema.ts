761e19cac7a8dcb52bc986243ca2a09febb7d176
MilanDev98 <132041534+PiramilanDev@users.noreply.github.com>
2023-11-09 17:03:44 +0530

    Edit Api

--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-export const createIssueSchema = z.object({
+export const issueSchema = z.object({
   title: z.string({ required_error: "Title is Required" }).min(1).max(255),
   description: z.string({ required_error: "Description is Required" }).min(1),
 });
