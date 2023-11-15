a66e4286cc5a08b8cc317ddb36b387ff6dca0d5a
MilanDev98 <132041534+PiramilanDev@users.noreply.github.com>
2023-11-15 12:54:59 +0530

    Issue Graph

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,27 @@
+import { Flex, Grid } from "@radix-ui/themes";
 import LatestIssues from "./LatestIssues";
+import IssueSummary from "./IssueSummary";
+import prisma from "~/prisma/client";
+import IssueChart from "./IssueChart";
 
-export default function Home() {
-  return <LatestIssues />;
+export default async function Home() {
+  const open = await prisma.issue.count({
+    where: { status: "OPEN" },
+  });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({
+    where: { status: "CLOSED" },
+  });
+
+  return (
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Flex direction="column" gap="5">
+        <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+        <IssueChart open={open} inProgress={inProgress} closed={closed} />
+      </Flex>
+      <LatestIssues />
+    </Grid>
+  );
 }
